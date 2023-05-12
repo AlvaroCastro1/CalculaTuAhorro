a42772ce8509ce81d943d4780882e94b8b76c320
AlvaroCastro1 <jesus.27castrop@gmail.com>
2023-05-11 19:27:44 -0600

    servidor activo: guardar bancos listo

--- src/proyecto/base/models.py
+++ src/proyecto/base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Banco(models.Model):
+    nombre = models.CharField(max_length=255)
+    tasa = models.DecimalField(max_digits=7, decimal_places=6)
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.nombre
--- src/proyecto/base/urls.py
+++ src/proyecto/base/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
 from . import views
-urlpatterns = [path('', views.lista_bancos, name='bancos')]
+urlpatterns = [
+    path('', views.lista_bancos, name='bancos'),
+    path('generar_bancos/', views.generar_bancos, name='generar_bancos'),
+    ]
