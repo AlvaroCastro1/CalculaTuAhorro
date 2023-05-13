64d935c729ccda8602bc4ef567b14659e045b1c4
AlvaroCastro1 <jesus.27castrop@gmail.com>
2023-05-12 21:04:38 -0600

    pagina de inicio

--- src/proyecto/base/urls.py
+++ src/proyecto/base/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import Lista_Bancos
+
 urlpatterns = [
-    path('', views.lista_bancos, name='bancos'),
+    path('', Lista_Bancos.as_view(), name='bancos'),
     path('generar_bancos/', views.generar_bancos, name='generar_bancos'),
     ]
