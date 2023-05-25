f0470359e6086017a7a680101a34864274b2a19a
AlvaroCastro1 <jesus.27castrop@gmail.com>
2023-05-24 21:14:53 -0600

    pagina de inicio

--- src/proyecto/base/urls.py
+++ src/proyecto/base/urls.py
@@ -4,8 +4,8 @@ from . import views
 from .views import Lista_Bancos, inicio
 
 urlpatterns = [
-    path('', Lista_Bancos.as_view(), name='bancos'),
+    path('GraficaEChart/', Lista_Bancos.as_view(), name='bancos'),
     path('generar_bancos/', views.generar_bancos, name='generar_bancos'),
-    path('inicio/', views.inicio, name='inicio'),
+    path('', views.inicio, name='inicio'),
     path('api/', include('api.urls'))
 ]
