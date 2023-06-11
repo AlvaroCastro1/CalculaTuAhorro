512a5723323004947e2cd32a1d0d1e23c3db6da2
AlvaroCastro1 <jesus.27castrop@gmail.com>
2023-06-11 11:53:03 -0600

    latest

--- src/proyecto/base/models.py
+++ src/proyecto/base/models.py
@@ -6,6 +6,8 @@ class Banco(models.Model):
     tasa = models.DecimalField(max_digits=7, decimal_places=6)
     creado = models.DateTimeField(auto_now_add=True)
     modificado = models.DateTimeField(auto_now=True)
+    rgb = models.CharField(max_length=10, default='default_value')
+    # imagen = models.ImageField(upload_to='banco_imagenes/')
     
     def __str__(self):
         return self.nombre
